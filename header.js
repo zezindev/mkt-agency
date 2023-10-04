bee816ff69685ac126d95d193984ed35a7524f08
José Lucas <joselucasskt@gmail.com>
2023-10-04 10:40:34 -0300

    Update

--- header.js
+++ header.js
@@ -1,7 +1,6 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header")
   if (window.scrollY > 100) {
-    // Mude para a quantidade de rolagem desejada
     header.classList.add("scrolled")
   } else {
     header.classList.remove("scrolled")
